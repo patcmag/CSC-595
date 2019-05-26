5a00f1ca83e10611f9e1d7ddcc77f4f3d43ea723
patcmag <49422432+patcmag@users.noreply.github.com>
2019-05-26 13:44:15 -0500

    Update visualizations.js
    Wrap MapVis and use drawMapVis() as function.

--- visualizations.js
+++ visualizations.js
@@ -1,44 +1,38 @@
-(function(){
-	var margin = {top: 50, left: 50, right: 50, bottom: 50},
-		height = 400 - margin.top - margin.bottom,
-		width = 800 - margin.left - margin.right;
+function MapVis() {
 
-	var width = 690, height = 500, centered;
+	var newMV = {
 
-	var svg = d3.select("#map")
-		.append("svg")
-		.attr("height", height)
-		.attr("width", width);
+		drawMapVis: function (svg, mapdata) {
 
-	var projection = d3.geoMercator()
-		.scale(50000)
-		.translate([76860, 40500]);
+			var projection = d3.geoMercator()
+				.scale(50000)
+				.translate([76860, 40500]);
 
-	var path = d3.geoPath().projection(projection);
+			var path = d3.geoPath().projection(projection);
 
-	var url = "city.topojson";
+			d3.json(mapdata, function (err, topology) {
+				console.log("topojson", topology)
+				var geojson = topojson.feature(topology, topology.objects.city);
+				console.log("geojson", geojson)
+				console.log("community", geojson.features)
 
 
-	d3.json(url, function(err, topology){
-		console.log("topojson", topology)
-		var geojson = topojson.feature(topology, topology.objects.city);
-		console.log("geojson", geojson)
-		console.log("community", geojson.features)
+				svg.selectAll("path").data(geojson.features).enter().append("path")
+					.attr("d", path).attr("fill", "steelblue")
+					.attr("d", path).attr("stroke", "black").attr("stroke-width", "0.2px")
 
+					.on("mouseover", function () {
+						d3.select(this).style("fill", "yellow")
+							.call(function (d) {
+								console.log(d);
+							})
+					})
+					.on("mouseout", function () {
+						d3.select(this).style("fill", "steelblue");
+					});
 
-		svg.selectAll("path").data(geojson.features).enter().append("path")
-			.attr("d", path).attr("fill", "steelblue")
-			.attr("d", path).attr("stroke", "black").attr("stroke-width", "0.2px")
-
-			.on("mouseover", function(){
-				d3.select(this).style("fill", "yellow")
-				.call(function (d){
-					console.log(d);
-				})
 			})
-			.on("mouseout", function(){
-				d3.select(this).style("fill", "steelblue");
-			})
-
-	})
-})();
+		}
+	};
+	return newMV;
+}
